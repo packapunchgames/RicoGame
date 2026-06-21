a4ea055d29a614e65d4fe966671b009c7c56806e
Edoardo Albanese <packapunch517@gmail.com>
2026-06-21 15:44:18 +0200

    Added gpgs
    Sign in only
    SHA1 debug:
    DE:B5:41:D2:5E:E4:16:08:CD:2B:44:05:07:C6:2B:F2:C7:54:18:D4

--- ScenesAndScripts/UI/start_screen.gd
+++ ScenesAndScripts/UI/start_screen.gd
@@ -1,6 +1,23 @@
 extends Control
 
+@onready var play_games_sign_in_client: PlayGamesSignInClient = $PlayGamesSignInClient
+
 @export var game_scene : PackedScene
+
+func _enter_tree() -> void:
+	GodotPlayGameServices.initialize()
+
+func _ready() -> void:
+	if not GodotPlayGameServices.android_plugin:
+		print("Plugin Not Found!")
+	else:
+		print("Main Menu")
+	
+	play_games_sign_in_client.is_authenticated()
+	play_games_sign_in_client.sign_in()
+
+func _on_play_games_sign_in_client_user_authenticated(is_authenticated: bool) -> void:
+	print("Hi from Godot! User is authenticated? %s" % is_authenticated)
 
 
 func _on_start_button_pressed() -> void:
